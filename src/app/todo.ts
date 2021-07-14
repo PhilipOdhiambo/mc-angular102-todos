b87d3d8f91841103d1bbe124802ae4f574e45551
Philip Owino <jofonowino@gmail.com>
2021-07-14 04:35:42 +0300

    add ability to filter todos

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,2 +1,13 @@
 export class Todo {
+    deadline:number
+    showDetail:boolean
+    constructor(
+        public category:string,
+        public description:string,
+    ) {
+        this.deadline = 7;
+        this.showDetail = false
+
+    }
 }
+
